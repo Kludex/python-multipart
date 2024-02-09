1394db9a30791fd9ac01ecf9be8f5cd3f14db2e7
lorenpike <noahe@metrized.com>
2024-02-09 06:50:11 -0800

    Added a comment clarifying a condition in parse_options_header

--- multipart/multipart.py
+++ multipart/multipart.py
@@ -110,6 +110,9 @@ def parse_options_header(value: Union[str, bytes]) -> Tuple[bytes, Dict[bytes, b
     options = {}
     for param in params:
         key, value = param
+        # If the value return from get_params() is a 3-tuple, the last
+        # element corresponds to the value.
+        # See: https://docs.python.org/3/library/email.compat32-message.html
         if isinstance(value, tuple):
             value = value[-1]
         # If the value is a filename, we need to fix a bug on IE6 that sends
