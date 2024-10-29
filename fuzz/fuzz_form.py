ee0784dd3d78c60bbb17d966b97eaddcdd39c95d
manunio <max.manu.nair@gmail.com>
2024-10-29 15:53:58 +0530

    fuzz: fix boundary error
    This will fix the 'Did not find boundary character' error/warning
    produced by `fuzz_form`, which will in turn increase fuzzing efficiency.

--- fuzz/fuzz_form.py
+++ fuzz/fuzz_form.py
@@ -29,13 +29,22 @@ def parse_form_urlencoded(fdp: EnhancedDataProvider) -> None:
 
 
 def parse_multipart_form_data(fdp: EnhancedDataProvider) -> None:
-    header = {"Content-Type": "multipart/form-data; boundary=--boundary"}
-    parse_form(header, io.BytesIO(fdp.ConsumeRandomBytes()), on_field, on_file)
+    boundary = 'boundary'
+    header = {"Content-Type": f"multipart/form-data; boundary={boundary}"}
+    body = (f"--{boundary}\r\n"
+            f"Content-Type: multipart/form-data; boundary={boundary}\r\n\r\n"
+            f"{fdp.ConsumeRandomString()}\r\n"
+            f"--{boundary}--\r\n")
+    parse_form(header, io.BytesIO(body.encode('latin1', errors='ignore')),
+               on_field, on_file)
 
 
 def TestOneInput(data: bytes) -> None:
     fdp = EnhancedDataProvider(data)
-    targets = [parse_octet_stream, parse_url_encoded, parse_form_urlencoded, parse_multipart_form_data]
+    targets = [
+        parse_octet_stream, parse_url_encoded, parse_form_urlencoded,
+        parse_multipart_form_data
+    ]
     target = fdp.PickValueInList(targets)
 
     try:
